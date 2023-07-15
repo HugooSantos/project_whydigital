a4b7c01937af36be33692f510a2e6301e4df60b6
hugo <hugogabrieltjj@gmail.com>
2023-07-15 18:50:29 -0300

    created migration of tasks

---
+++ backend/database/migrations/2023_07_15_214457_create_tasks.php
@@ -0,0 +1,23 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration
+{
+    public function up(): void
+    {
+        Schema::create('tasks', function (Blueprint $table) {
+            $table->id();
+            $table->string('description_task', 255);
+            $table->char('complete_task',1);
+            $table->integer('user_id_task');
+        });
+    }
+
+    public function down(): void
+    {
+        Schema::dropIfExists('tasks');
+    }
+};
--- backend/routes/api.php
+++ backend/routes/api.php
@@ -4,3 +4,4 @@ use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 
+Route::post('/users', [UserController::class, 'createUser']);
