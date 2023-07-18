a7c01bf19c093ad3608277810336ccfc0a9a0276
hugo <hugogabrieltjj@gmail.com>
2023-07-17 23:21:42 -0300

    made some changes to seed database users

---
+++ backend/database/seeders/UserSeeder.php
@@ -0,0 +1,42 @@
+<?php
+
+namespace Database\Seeders;
+
+use App\Models\User;
+use Illuminate\Database\Seeder;
+
+class UserSeeder extends Seeder
+{
+    public function run(): void
+    {
+        $users = $this->getArrayUsersToCreate();
+        foreach ($users as $user) {
+            User::create([
+                "name" => $user['name'],
+                "email" => $user['email'],
+                "password" => $user['password']
+            ]);
+        }
+    }
+
+    private function getArrayUsersToCreate()
+    {
+        return [
+            [
+                "name" => "Eduardo",
+                "email" => "eduardo@whydigital.com",
+                "password" => "12345678"
+            ],
+            [
+                "name" => "Bruno",
+                "email" => "bruno@whydigital.com",
+                "password" => "12345678"
+            ],
+            [
+                "name" => "Hugo",
+                "email" => "hugo@whydigital.com",
+                "password" => "12345678"
+            ]
+        ];
+    }
+}
--- runProject.sh
+++ runProject.sh
@@ -1,2 +1,4 @@
 cd backend/ ; docker-compose up -d
 cd ../frontend ; docker-compose up -d
+docker exec backend-whydigital php artisan migrate --force
+docker exec backend-whydigital php artisan db:seed
